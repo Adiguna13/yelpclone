fabb10794986c55229adf9358a71dfc347dc8bcd
Adiguna <adigunarizqi@gmail.com>
2023-08-21 10:50:13 +0700

    set up model place

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const app = express();
+
+//models
+const Place = require("./models/place");
+
+mongoose
+  .connect("mongodb://127.0.0.1/bestpoints")
+  .then((result) => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/seed/place", async (req, res) => {
+  const place = new Place({
+    title: "State Building",
+    price: "$5252525",
+    description: "A Building",
+    location: "Manchester",
+  });
+
+  await place.save();
+  res.send(place);
 });
 
 app.listen(3000, () => {
