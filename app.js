bf9b5b59f68c788ff8b26ed54d1f35a5d7a2d34d
Adiguna <adigunarizqi@gmail.com>
2023-09-03 07:02:08 +0700

    setup session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const ejsMate = require("ejs-mate");
 const express = require("express");
+const session = require("express-session");
 const ErrorHandler = require("./utils/ErrorHandler");
 // const Joi = require("joi");
 const methodOverride = require("method-override");
@@ -27,6 +28,18 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "secret-key",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+      maxAge: +1000 * 60 * 60 * 24 * 7,
+    },
+  })
+);
 
 app.get("/", (req, res) => {
   res.render("home");
