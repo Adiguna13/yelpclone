8c06857f9b33ba96550c8c376fb6f2d600682a46
Adiguna <adigunarizqi@gmail.com>
2023-09-10 10:55:16 +0700

    implement login with isAuth middleware

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-  if (req.isAuthenticated()) {
+  if (!req.isAuthenticated()) {
     req.flash("error_msg", "you are not logged in");
     res.redirect("/login");
   }
