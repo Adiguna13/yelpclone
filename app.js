bfca48766e2b20e3dd76903b84b8dbbc2310db9f
Adiguna <adigunarizqi@gmail.com>
2023-09-11 10:48:10 +0700

    helper to get data user after login

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ passport.serializeUser(User.serializeUser()); // setter
 passport.deserializeUser(User.deserializeUser()); // getter (ambil data yang di serialize oleh user)
 
 app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
   res.locals.success_msg = req.flash("success_msg");
   res.locals.error_msg = req.flash("error_msg");
   next();
