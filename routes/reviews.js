b3e4349a64a8b8d2daf23ee7425099823010b169
Rizqi Adiguna <adigunarizqi@gmail.com>
2023-09-18 11:45:07 +0700

    add author to review models and hide form review when not login

--- routes/reviews.js
+++ routes/reviews.js
@@ -24,10 +24,12 @@ router.post(
   isValidObjectId("/places"),
   validateReview,
   wrapAsync(async (req, res) => {
+    const {place_id} = req.params;
     const review = new Review(req.body.review);
+    review.author = req.user._id;
+    await review.save();
     const place = await Place.findById(req.params.place_id);
     place.reviews.push(review);
-    await review.save();
     await place.save();
     req.flash("success_msg", "Review added successfully");
     res.redirect(`/places/${req.params.place_id}`);
